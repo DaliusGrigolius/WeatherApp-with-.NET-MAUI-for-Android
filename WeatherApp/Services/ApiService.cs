e485c6e642c4a803a2b5938c5335cbe6cbd37480
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-12-17 13:53:22 +0200

    Add project files.

---
+++ WeatherApp/Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public static class ApiService
+    {
+        public static async Task<Root> GetWeather(double latitude, double longitude)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync($"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&appid=YOUR_API_KEY_HERE");
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+
+        public static async Task<Root> GetWeatherByCity(string city)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync($"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid=YOUR_API_KEY_HERE");
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
